2c611751d6001696caed5218e3ed0e109290fd8d
Yu-Ren Zhang <e8035669@gmail.com>
2024-08-08 20:43:20 +0800

    add armv7l config

--- recipe/get_cpu_triplet.sh
+++ recipe/get_cpu_triplet.sh
@@ -1,5 +1,6 @@
 get_triplet() {
   local CPU_ARCH
+  post_arch=''
   if [[ "$1" == "linux-64" ]]; then
     CPU_ARCH="x86_64"
   elif [[ "$1" == "linux-ppc64le" ]]; then
@@ -8,9 +9,12 @@ get_triplet() {
     CPU_ARCH="aarch64"
   elif [[ "$1" == "linux-s390x" ]]; then
     CPU_ARCH="s390x"
+  elif [[ "$1" == "linux-armv7l" ]]; then
+    CPU_ARCH="armv7l"
+    post_arch='eabihf'
   else
     echo "Unknown architecture"
     exit 1
   fi
-  echo $CPU_ARCH-conda-linux-gnu
+  echo $CPU_ARCH-conda-linux-gnu${post_arch}
 }
